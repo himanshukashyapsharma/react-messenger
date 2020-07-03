5d1784b03b436d75de6096fbf704ee4e10f58242
himanshukashyapsharma <himanshukashyapsharma@gmail.com>
2020-07-03 20:24:15 +0530

    existing user and room terminated bug fix

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -16,6 +16,7 @@ const Chat = ({ location })=>{
     const [room,setRoom] = useState("")
     const [message,setMessage] = useState("")
     const [messages,setMessages] = useState([])
+    // const ENDPOINT = "http://localhost:5000/"
     const ENDPOINT = "https://kashyaps-messenger.herokuapp.com/"
 
     useEffect(()=>{
